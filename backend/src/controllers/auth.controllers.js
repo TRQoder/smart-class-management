8662be234a5e4d868bff182e0704b2a7e3668fd1
Tarique <mdtariqueh1@gmail.com>
2025-09-05 21:20:26 +0530

    .

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -155,7 +155,7 @@ const registerStudent = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // use secure cookies in production
-      sameSite: "Lax",
+      sameSite: isProduction ? "None" : "Lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
