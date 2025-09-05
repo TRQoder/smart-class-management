01faa741cc9dce028015789c7805df01ca51c8e2
Tarique <mdtariqueh1@gmail.com>
2025-09-05 21:38:45 +0530

    fixed error login

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -42,11 +42,12 @@ const login = async (req, res) => {
     );
 
     // 5. Set cookie
+    const isProduction = process.env.NODE_ENV === "production";
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Lax",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
+      secure: isProduction, // use secure cookies in production
+      sameSite: isProduction ? "None" : "Lax",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
     // 6. Response
